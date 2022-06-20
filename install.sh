f261ee1e5097675ae08355c5e1e17053b4fcec83
thesaurabhdua <62702230+thesaurabhdua@users.noreply.github.com>
2022-06-21 04:00:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
+sudo docker run --name docker-nginx -p 80:80 nginx
